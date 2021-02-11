a996bba829714d907734d901c6be915a8a84cddd
mfederi <federici.marco.94@gmail.com>
2021-02-11 18:13:29 +0100

    Caching system for single samples added

--- datasets/cmnist.py
+++ datasets/cmnist.py
@@ -5,7 +5,7 @@ from torchvision.datasets import MNIST
 
 from datasets.cmnist_dist import make_joint_distribution
 
-
+# Implementation of the CMNIST, d-CMNIST and y-CMNIST datasets for pytorch
 class CMNIST(MNIST):
     def __init__(self, root, version='CMNIST', sample_once=False, **params):
         super(CMNIST, self).__init__(root=root, **params, transform=ToTensor())
@@ -17,15 +17,22 @@ class CMNIST(MNIST):
         self.sampled_data = {}
 
     def __getitem__(self, index):
-        x, d = super(CMNIST, self).__getitem__(index)
+        if index in self.sampled_data:
+            data = self.sampled_data[index]
+        else:
+            x, d = super(CMNIST, self).__getitem__(index)
 
-        sample = self.dist.condition_on('d', d).sample()
+            # sample from p(e,y,c|d) to determine color, label and environment
+            sample = self.dist.condition_on('d', d).sample()
 
-        # Concatenate an empty channel (red)
-        x = torch.cat([x, x * 0], 0)
+            # Concatenate an empty channel (red)
+            x = torch.cat([x, x * 0], 0)
 
-        # If the color is 1, make the empty channel the first (green)
-        if sample['c'] == 1:
-            x = torch.roll(x, 1, 0)
+            # If the color is 1, make the empty channel the first (green)
+            if sample['c'] == 1:
+                x = torch.roll(x, 1, 0)
 
-        return {'x': x, 'y': sample['y'], 'e': sample['e']}
+            data = {'x': x, 'y': sample['y'], 'e': sample['e']}
+        if self.sample_once:
+            self.sampled_data[index] = data
+        return data
