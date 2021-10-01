fbc1be50fb56fef2911bfd7476905551058d43fa
mfederi <federici.marco.94@gmail.com>
2021-10-01 11:13:47 +0200

    updated evaluation timing

--- src/callbacks/evaluation_callbacks.py
+++ src/callbacks/evaluation_callbacks.py
@@ -4,12 +4,13 @@ import pytorch_lightning as pl
 import numpy as np
 
 from src.callbacks.timed_callback import TimedCallback
-from src.loggers.log_entry import SCALARS_ENTRY, LogEntry
+from src.loggers.log_entry import SCALARS_ENTRY, LogEntry, SCALAR_ENTRY
 
 
 class EvaluationCallback(TimedCallback):
     # Only one unit is currently supported: `10 minutes 10 seconds` is not a valid value
-    def __init__(self, name, evaluator, evaluate_every, log_end=True, log_beginning=True, pause_timers=True):
+    def __init__(self, name, evaluator, evaluate_every, log_end=True, log_beginning=True, pause_timers=True,
+                 log_evaluation_time=True):
         super(EvaluationCallback, self).__init__(time=evaluate_every)
 
         self.evaluator = evaluator
@@ -18,8 +19,10 @@ class EvaluationCallback(TimedCallback):
         self.log_end = log_end
         self.log_beginning = log_beginning
         self.pause_timers = pause_timers
+        self.log_evaluation_time = log_evaluation_time
 
     def evaluate(self, pl_module: pl.LightningModule):
+        time_before = time.time()
         log_entry = self.evaluator.evaluate(pl_module)
         if hasattr(pl_module, 'counters'):
             counters = pl_module.counters
@@ -31,6 +34,11 @@ class EvaluationCallback(TimedCallback):
             global_step=pl_module.trainer.global_step,
             counters=counters
         )
+        if self.log_evaluation_time:
+            pl_module.trainer.logger.log(
+                name='EvaluationTime/%s' % self.name,
+                log_entry=LogEntry(time.time()-time_before, SCALAR_ENTRY)
+            )
 
     def on_train_start(self, trainer: 'pl.Trainer', pl_module: 'pl.LightningModule') -> None:
         self.start_timer()
@@ -51,9 +59,7 @@ class EvaluationCallback(TimedCallback):
                     if isinstance(callback, TimedCallback):
                         callback.stop_timer()
 
-            last_time = time.time()
             self.evaluate(pl_module)
-            pl_module.log('EvaluationTime/%s' % self.name, time.time()-last_time)
 
             if self.pause_timers:
                 # Restart the timers
